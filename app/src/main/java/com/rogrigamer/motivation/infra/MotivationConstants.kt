606c07fa7323d96fb22b8c8f0e895ed25099d5b5
Rogri <rogri0307@hotmail.com>
2022-04-09 20:26:57 -0300

    add const in variables

--- app/src/main/java/com/rogrigamer/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/rogrigamer/motivation/infra/MotivationConstants.kt
@@ -3,12 +3,12 @@ package com.rogrigamer.motivation.infra
 class MotivationConstants private constructor() {
 
     object KEY {
-        val PERSON_NAME = "name"
+        const val PERSON_NAME = "name"
     }
 
     object PHRASEFILTER {
-        val ALL = 1
-        val HAPPY = 2
-        val MORNING = 3
+        const val ALL = 1
+        const val HAPPY = 2
+        const val MORNING = 3
     }
 }
