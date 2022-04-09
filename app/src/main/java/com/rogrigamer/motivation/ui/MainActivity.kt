b1d7a9322258bba23c461e5cca4c723e89d692cb
Rogri <rogri0307@hotmail.com>
2022-04-09 20:29:09 -0300

    implementation view binding, remove actionBar.hide and update getcolor

--- app/src/main/java/com/rogrigamer/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/rogrigamer/motivation/ui/MainActivity.kt
@@ -2,57 +2,46 @@ package com.rogrigamer.motivation.ui
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 import com.rogrigamer.motivation.R
+import com.rogrigamer.motivation.databinding.ActivityMainBinding
 import com.rogrigamer.motivation.infra.MotivationConstants
 import com.rogrigamer.motivation.infra.SecurityPreferences
 import com.rogrigamer.motivation.repository.Mock
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var binding: ActivityMainBinding
+
     private var mPhraseFilter: Int = MotivationConstants.PHRASEFILTER.ALL
-    private var textName: TextView? = null
-    private var buttonNewPhrase: AppCompatButton? = null
-    private var imageAll: ImageView? = null
-    private var imageHappy: ImageView? = null
-    private var imageMorning: ImageView? = null
-    private var textPhrase: TextView? = null
 
     private lateinit var mSecurutyPreferences: SecurityPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        textName = findViewById(R.id.textName)
-        buttonNewPhrase = findViewById(R.id.buttonNewPhrase)
-        imageAll = findViewById(R.id.imageAll)
-        imageHappy = findViewById(R.id.imageHappy)
-        imageMorning = findViewById(R.id.imageMorning)
-        textPhrase = findViewById(R.id.textPhrase)
-
-        buttonNewPhrase?.setOnClickListener(this)
-        imageAll?.setOnClickListener(this)
-        imageHappy?.setOnClickListener(this)
-        imageMorning?.setOnClickListener(this)
-
-
-        if (supportActionBar != null) {
-            supportActionBar!!.hide()
-        }
+        binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageMorning.setOnClickListener(this)
 
         mSecurutyPreferences = SecurityPreferences(this)
         val name = mSecurutyPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
-        textName?.text = "olá, $name!"
+        val textHelloName = "olá, $name!"
+        binding.textName.text = textHelloName
 
         //Lógica inicial de seleção
-        imageAll?.setColorFilter(resources.getColor(R.color.colorAccent))
+        binding.imageAll.setColorFilter(
+            ContextCompat.getColor(
+                applicationContext,
+                R.color.colorAccent
+            )
+        )
         handleNewPhrase()
     }
-
 
     override fun onClick(view: View) {
         val id = view.id
@@ -67,29 +56,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun handleFilter(id: Int) {
 
-        imageAll?.setColorFilter(resources.getColor(R.color.white))
-        imageHappy?.setColorFilter(resources.getColor(R.color.white))
-        imageMorning?.setColorFilter(resources.getColor(R.color.white))
+        with(binding) {
+            imageAll.setColorFilter(ContextCompat.getColor(applicationContext, R.color.white))
+            imageHappy.setColorFilter(ContextCompat.getColor(applicationContext, R.color.white))
+            imageMorning.setColorFilter(ContextCompat.getColor(applicationContext, R.color.white))
+        }
 
         when (id) {
             R.id.imageAll -> {
-                imageAll?.setColorFilter(resources.getColor(R.color.colorAccent))
+                binding.imageAll.setColorFilter(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.colorAccent
+                    )
+                )
                 mPhraseFilter = MotivationConstants.PHRASEFILTER.ALL
             }
             R.id.imageHappy -> {
-                imageHappy?.setColorFilter(resources.getColor(R.color.colorAccent))
+                binding.imageHappy.setColorFilter(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.colorAccent
+                    )
+                )
                 mPhraseFilter = MotivationConstants.PHRASEFILTER.HAPPY
             }
             R.id.imageMorning -> {
-                imageMorning?.setColorFilter(resources.getColor(R.color.colorAccent))
+                binding.imageMorning.setColorFilter(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.colorAccent
+                    )
+                )
                 mPhraseFilter = MotivationConstants.PHRASEFILTER.MORNING
             }
         }
-
-
     }
 
     private fun handleNewPhrase() {
-        textPhrase?.text = Mock().getPhrase(mPhraseFilter)
+        binding.textPhrase.text = Mock().getPhrase(mPhraseFilter)
     }
 }
